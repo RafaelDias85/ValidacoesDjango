2b94305d6264ca5728851ea1f61b568a5a43887a
Bowie <rafaeldiassilv@gmail.com>
2024-02-13 22:50:36 -0300

    mais algumas alterações

--- clientes/migrations/0001_initial.py
+++ clientes/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
                 ('nome', models.CharField(max_length=100)),
                 ('email', models.EmailField(max_length=30)),
                 ('cpf', models.CharField(max_length=11, unique=True)),
-                ('rg', models.CharField(max_length=9)),
+                ('rg', models.CharField(max_length=9, unique=True)),
                 ('celular', models.CharField(max_length=14)),
                 ('ativo', models.BooleanField()),
             ],
