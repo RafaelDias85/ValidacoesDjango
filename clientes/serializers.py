1b05dc1a296e3ce85edeb9cb717f493c172885ce
Bowie <rafaeldiassilv@gmail.com>
2024-02-14 22:15:28 -0300

    alterações para Validação

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -6,3 +6,19 @@ class ClienteSerializer(serializers.ModelSerializer):
         model = Cliente
         fields = '__all__'
     
+    def validate_cpf (self, cpf):
+        if len(cpf) != 11:
+            raise serializers.ValidationError("O CPF deve ter 11 dígitos")
+        return cpf
+    
+    def validate_nome (self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError("Não inclua números neste campo")
+        
+        return nome
+    
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError ("O RG deve ter 9 digitos")
+        return rg
+    
